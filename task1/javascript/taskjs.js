5c2d19df27e7926ec57b7fdaff6683216bcc4ca1
arun <aramegowdaostb1@gmail.com>
2019-05-20 16:29:20 +0530

    javascript task1

---
+++ task1/javascript/taskjs.js
@@ -0,0 +1,5 @@
+var a ="Hello world", b="welcome",c="javascript" ,d="to";
+var a1 ="Hi good afternon";
+var c1="Had your lunch";
+
+document.write (a+"<br>"+b+"<br>"+c+"<br>"+a+' '+b+''+d+''+c+"<br>"+a1+' '+c1);
